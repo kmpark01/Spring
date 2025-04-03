129eb889f044f39bf0e611167c31e5d94bc981ac
unknown <rudalsfive@naver.com>
2025-04-03 17:37:19 +0900

    추가

---
+++ chapter06_AOP/src/main/java/org/joonzis/service/SampleServiceimpl.java
@@ -0,0 +1,14 @@
+package org.joonzis.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleServiceimpl implements SampleService {
+
+	@Override
+	public int doAdd(String str1, String str2) throws Exception {
+		
+		return Integer.parseInt(str1) + Integer.parseInt(str2);
+	}
+	
+}	
